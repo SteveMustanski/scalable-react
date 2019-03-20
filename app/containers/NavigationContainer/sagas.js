52ef4a71fe9759f2588d31eb045f75d148df69e5
SteveMustanski <steve.mustanski@gmail.com>
2019-03-19 23:50:20 -0400

    use saga to get data from server

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,11 +1,33 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 
+import { takeLatest } from 'redux-saga';
+import { call, put } from 'redux-saga/effects';
+import {
+  REQUEST_TOPICS,
+  requestTopicsSucceeded,
+  requestTopicsFailed,
+} from './actions';
+
+export function fetchTopicsFromServer() {
+  console.log('fetching');
+  return fetch('http://localhost:3000/api/topics').then(
+    response => response.JSON,
+  );
+}
+
+function* fetchTopics() {
+  try {
+    const topics = yield call(fetchTopicsFromServer);
+    yield put(requestTopicsSucceeded(topics));
+  } catch (e) {
+    yield put(requestTopicsFailed(e.message));
+  }
+}
+
 // Individual exports for testing
-export function* defaultSaga() {
-  return;
+export function* fetchTopicsSaga() {
+  yield* takeLatest(REQUEST_TOPICS, fetchTopics);
 }
 
 // All sagas to be loaded
-export default [
-  defaultSaga,
-];
+export default [fetchTopicsSaga];
